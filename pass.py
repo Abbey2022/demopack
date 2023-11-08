4aa457a1c8107c83cfc2f94885f96b0169dff205
Abbey2022 <megaabbey2@gmail.com>
2023-11-08 17:30:10 -0500

    mypython class

---
+++ pass.py
@@ -0,0 +1,17 @@
+count = 0
+while count < 10:
+    n = int(input('enter a number' ))
+    #if n % 3 == 0 :
+        #pass
+    #else:
+       # print(n)
+    #count += 1  
+    if n % 3 != 0:
+        print(n)
+    count +=1
+
+
+
+
+
+
